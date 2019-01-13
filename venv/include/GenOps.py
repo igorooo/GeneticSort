abd9ade7581226fb7da6fb2d9d1f6f45ea6cdd6f
igorooo <igorczeczot@windowslive.com>
2019-01-13 18:46:38 +0100

    12.01 20:00

--- venv/include/GenOps.py
+++ venv/include/GenOps.py
@@ -8,6 +8,7 @@ class GenObject(object):
         self.i_ARR_LEN = len(ar_ARRAY)
         self.ar_MOVES = []
         self.i_FITNESS = 0
+        self.ar_RES_ARR = ar_ARRAY[:]
 
     def __lt__(self, other):
         return self.i_FITNESS < other.i_FITNESS
@@ -16,31 +17,31 @@ class GenObject(object):
     def create_randomly(self):
         for i in range(0,self.i_ARR_LEN):
             self.ar_MOVES.append( randint( 0 , self.i_ARR_LEN-1 ) )
-        self.ar_RES_ARR = ar_ARRAY[:]
+        self.ar_RES_ARR = self.ar_ARRAY[:]
         self.implement_moves()
         self.i_FITNESS = i_check_array_fitness(self.ar_RES_ARR)
 
     def mutation(self):
-        for ELEM in self.ar_MOVES:
+        for i in range(0, len(self.ar_MOVES)):
             if (randint(0, 100) < 5):
-                ELEM = randint( 0 , self.i_ARR_LEN-1 )
+                self.ar_MOVES[i] = randint( 0 , self.i_ARR_LEN-1 )
+
 
     def crossover(self,MOTHER,FATHER):
-        RAND_1 = randint(0,self.i_ARR_LEN)
-        RAND_2 = randint(0,self.i_ARR_LEN)
+        RAND_1 = randint(0,self.i_ARR_LEN-1)
+        RAND_2 = randint(0,self.i_ARR_LEN-1)
         
-        SEP_A = min(RAND_1,RAND_2)
-        SEP_B = max(RAND_1,RAND_2)
-        
-        for i in range(0, self.i_ARR_LEN):
-            if(i < SEP_A):
-                self.ar_MOVES[i] = MOTHER.ar_MOVES[i]
-                continue
-            if(i < SEP_B):
-                self.ar_MOVES[i] = FATHER.ar_MOVES[i]
-                continue
-            else:
-                self.ar_MOVES[i] = MOTHER.ar_MOVES[i]
+        #SEP_A = min(RAND_1,RAND_2)
+        #SEP_B = max(RAND_1,RAND_2)
+
+        SEP_A = RAND_1
+
+
+        for i in range(0, SEP_A):
+            self.ar_MOVES.append(MOTHER.ar_MOVES[i])
+        for i in range(SEP_A, self.i_ARR_LEN):
+            self.ar_MOVES.append(FATHER.ar_MOVES[i])
+
         self.mutation()
         self.implement_moves()
         self.i_FITNESS = i_check_array_fitness(self.ar_RES_ARR)
@@ -77,6 +78,7 @@ class GenSort(object):
         self.ar_SND_GEN = []
         self.f_LOGS = open("logs.txt","w")
         self.f_LOGS.close()
+        self.i_NO_GEN = 0
 
         if(self.i_POP_SIZE % 2 == 1):
             self.i_POP_SIZE += 1
@@ -87,10 +89,11 @@ class GenSort(object):
             TEMP = GenObject(self.ar_ARRAY)
             TEMP.create_randomly()
             self.ar_FST_GEN.append(TEMP)
+        self.save_logs_from_fstgen()
 
     def get_parent(self):
-        RANDOM_1 = self.ar_FST_GEN[randint(0, self.i_POP_SIZE)]
-        RANDOM_2 = self.ar_FST_GEN[randint(0, self.i_POP_SIZE)]
+        RANDOM_1 = self.ar_FST_GEN[randint(0, self.i_POP_SIZE-1)]
+        RANDOM_2 = self.ar_FST_GEN[randint(0, self.i_POP_SIZE-1)]
 
         if( RANDOM_1 < RANDOM_2):
             return RANDOM_2
@@ -98,7 +101,8 @@ class GenSort(object):
             return RANDOM_1
 
     def next_gen(self):
-        for i in range(0, self.i_POP_SIZE/2):
+        self.ar_SND_GEN = []
+        for i in range(0, int(self.i_POP_SIZE/2)):
             CHILD_A = GenObject(self.ar_ARRAY)
             CHILD_B = GenObject(self.ar_ARRAY)
 
@@ -108,10 +112,36 @@ class GenSort(object):
             CHILD_A.crossover(PARENT_A,PARENT_B)
             CHILD_B.crossover(PARENT_B,PARENT_A)
 
-            self.ar_FST_GEN.append( CHILD_A )
+            self.ar_SND_GEN.append( CHILD_A )
             self.ar_SND_GEN.append( CHILD_B )
 
-        self.ar_FST_GEN = self.ar_SND_GEN
+        self.ar_FST_GEN = []
+        self.ar_FST_GEN = self.ar_SND_GEN[:]
+        self.save_logs_from_fstgen()
+
+
+    def save_logs_from_fstgen(self):
+        # saving generation in logs.txt file
+        self.f_LOGS = open("logs.txt","a")
+        for ELEM in self.ar_FST_GEN:
+            self.f_LOGS.write("{")
+            for i in range(0,len(ELEM.ar_RES_ARR)):
+                self.f_LOGS.write(str(ELEM.ar_RES_ARR[i]))
+                self.f_LOGS.write(" ")
+            self.f_LOGS.write("}")
+            self.f_LOGS.write("(")
+            for i in range(0,len(ELEM.ar_MOVES)):
+                self.f_LOGS.write(str(ELEM.ar_MOVES[i]))
+                self.f_LOGS.write(" ")
+            self.f_LOGS.write(")")
+            self.f_LOGS.write("[")
+            self.f_LOGS.write(str(ELEM.i_FITNESS))
+            self.f_LOGS.write("]")
+            self.f_LOGS.write(" ")
+        self.f_LOGS.write("\n")
+        self.f_LOGS.close()
+
+
 
 
 
--- venv/include/Main.py
+++ venv/include/Main.py
@@ -2,15 +2,24 @@ from ArrayOps import *
 from GenOps import *
 
 
-ARR = ar_array_gen(10)
+#ARR = ar_array_gen(10)
 
-for c in ARR:
-    print(c)
+ARR = [5,4,2,1,10,9,8,3,7,6]
+
+#for c in ARR:
+ #   print(c)
 
 VAL = i_check_array_fitness(ARR)
 
 
-tObj = GenObject(ARR)
+GSORT = GenSort(ARR,10)
+
+GSORT.create_fst_gen()
+
+for i in range(0,10000):
+    GSORT.next_gen()
+
+
 
 
 
